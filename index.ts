8d184a9f0923bfb69aeaedf8df3fc69d8070f58a
Udit Karode <udit.karode@gmail.com>
2021-07-10 17:33:01 +0530

    [fix:index] export necessary components

---
+++ index.ts
@@ -0,0 +1,17 @@
+export * from "./drytype.ts";
+export * from "./utils.ts";
+export { ValidationError } from "./validation-error.ts";
+export * from "./modules/Array.ts";
+export * from "./modules/Record.ts";
+export * from "./modules/String.ts";
+export * from "./modules/StringExtensions.ts";
+export * from "./modules/primitives/BigInt.ts";
+export * from "./modules/primitives/Boolean.ts";
+export * from "./modules/primitives/Function.ts";
+export * from "./modules/primitives/Number.ts";
+export * from "./modules/primitives/Object.ts";
+export * from "./modules/primitives/String.ts";
+export * from "./modules/primitives/Symbol.ts";
+export * from "./modules/primitives/Undefined.ts";
+
+export type { ValidationResult } from "./utils.ts";
